60e300d24c531b8fd1123aee2efb93dd7b98d9b8
Birkir Oskarsson <birkiro@gmail.com>
2020-08-18 08:44:59 +0200

    Update and rename lininterep.c to lininterp.c

--- lininterp/lininterp.c
+++ lininterp/lininterp.c
@@ -1,18 +1,23 @@
 #include <stdbool.h>
-
 #include "lininterp.h"
+
+float interpolate(Point2 p0, Point2 p1, float sp)
+{
+    return (p0.y + ((sp - p0.x) / (p1.x - p0.x)) * (p1.y - p0.y));
+}
 
 int lininterp(float x[TRIGGERS], float y[TRIGGERS], float sp, float* res)
 {
+    int i;
     Point2 p0 = {0,0};
     Point2 p1 = {0,0};
 
     // Check if x is increasing
-    for (int i = 0; i < (TRIGGERS - 1); i++)
+    for (i = 0; i < (TRIGGERS - 1); i++)
         if (x[i] >= x[i+1]) return ERRXORDER;
 
     // check if the setpoint hits any of the x points
-    for (int i = 0; i < TRIGGERS; i++)
+    for (i = 0; i < TRIGGERS; i++)
     {
         if (sp == x[i])
         {
@@ -35,7 +40,7 @@ int lininterp(float x[TRIGGERS], float y[TRIGGERS], float sp, float* res)
     else // interpolation is needed
     {
         bool done = false;
-        for (int i = 1; i < TRIGGERS; i++)
+        for (i = 1; i < TRIGGERS; i++)
         {
             if (sp < x[i] && !done)
             {
@@ -49,9 +54,4 @@ int lininterp(float x[TRIGGERS], float y[TRIGGERS], float sp, float* res)
     *res = interpolate(p0, p1, sp);
 
     return NOERROR;
-}
-
-float interpolate(Point2 p0, Point2 p1, float sp)
-{
-    return (p0.y + ((sp - p0.x) / (p1.x - p0.x)) * (p1.y - p0.y));
 }
